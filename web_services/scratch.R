d0fda7a5f01d7346bbcae61e50c0389ec89bbf71
Andy Lathrop <alathrop@blue-granite.com>
2017-06-14 15:02:35 -0400

    update scratch.R

--- web_services/scratch.R
+++ web_services/scratch.R
@@ -1,3 +1,4 @@
+
 # reference <https://msdn.microsoft.com/en-us/microsoft-r/scaler-spark-getting-started>
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -49,10 +50,15 @@ POC_data_root <- "/share" # HDFS location of the example data
 rxHadoopListFiles(POC_data_root)
 
 # from kbmMain erg_risk46 ~ erg_risk03 + age_gender + county_name
+
+# create rx Hive data source
 trainHive <- RxHiveData(query = "select erg_risk46, erg_risk03, age_gender, county_name from sample_table")
 
+# initialize xdf data source. On HDFS, make sure that the file parameter is a path, not a filename
 trainXdf <- RxXdfData(file = "/my_HDFS_data_path",
-                      fileSystem = myHdfsFileSystem1)
+                      fileSystem = myHdfsFileSystem)
+
+# write xdf file to HDFS
 rxImport(inData = trainHive, outFile = trainXdf) 
 
 # ~~~~~~~~~~~
